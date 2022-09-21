42c8935d7dff3f3a833499fb47630cfc88bb8ffb
Tim Tse <timtse76@gmail.com>
2022-09-21 18:26:31 -0400

    added next auth config

---
+++ client/pages/api/auth/[...nextauth].js
@@ -0,0 +1,53 @@
+import { authenticate } from "api/login";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions = {
+  session: {
+    strategy: 'jwt',
+    maxAge: 3000
+  },
+  providers: [
+    CredentialsProvider({
+      id: 'credentials',
+      name: 'Credentials',
+      credentials: {
+        username: { type: 'text', label: 'Username'},
+        password: { type: 'text', label: 'Password' }
+      },
+      async authorize(credentials) {
+        const res = await authenticate(credentials);
+
+        if ((res.status && res.status !== 200) || (res.authenticated)) {
+          return null;
+        }
+
+        return res.user;
+      }
+    })
+  ],
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: '/login',
+    error: '/login'
+  },
+  callbacks: {
+    async jwt({ token, user, account }) {
+      if (account && user) {
+        return {
+          ...token,
+          user: user
+        }
+      }
+
+      return token;
+    },
+    async session({ session, token}) {
+      session.user = token.user;
+      
+      return session;
+    }
+  }
+};
+
+export default NextAuth(authOptions);
