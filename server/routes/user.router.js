50608a18a379eb1c6b33e2186b2d0812c33bb5d9
Tim Tse <timtse76@gmail.com>
2022-09-21 18:17:05 -0400

    added authenticate endpoint

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const userHandler = require('../handlers/user.handler');
 
 router.post('/createuser', userHandler.createUser);
+router.post('/authenticate', userHandler.authenticateUser);
 
 module.exports = router;
