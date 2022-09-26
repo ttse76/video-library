77f003fc8130bdb314bc3d9a05a2f76df62f1543
Tim Tse <timtse76@gmail.com>
2022-09-26 18:05:43 -0400

    started navbar

---
+++ client/src/components/document/Navigation/index.js
@@ -0,0 +1,12 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+
+export default function Navigation() {
+  return (
+    <Navbar expand="lg" variant="light" bg="light">
+      <Container>
+        
+      </Container>
+    </Navbar>
+  )
+}
